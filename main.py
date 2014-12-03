f4b38abeecb30cd7e692de5fb3c11011af4d91b4
Chris <cwalter@ledaenergy.com>
2014-12-03 10:20:22 -0800

    step 1: compiles

--- main.py
+++ main.py
@@ -8,10 +8,17 @@ def main(T):
 	data = getData()
 	features = data['features']
 	labels = [(data['labels'][i], features[i]) for i in range(len(features))]
+	count = 0
+	featureIndices = []
 
 	while(count < T):
 		labelSubset = getSubset(labels)
+		tree = buildDecTree(labelSubset)
+		featureIndices.append(tree[1])
+		print(tree[1])
 		count+=1
+
+	print(featureIndices)
 	
 def getSubset(labels):
 	# labels = list of tuples: (labelVal = 0 or 1, [featureVals])
@@ -19,17 +26,19 @@ def getSubset(labels):
 	n = len(labels)
 
 	for i in range(len(labels)):
-		result.push(labels[random.randint(0, n-1)])
+		result.append(labels[random.randint(0, n-1)])
 	return result
+
 def getData():
 	result = {'features': [], 'labels': []}
 
-	with open('trainFeatures.csv', 'rb') as csvfile:
-		features = csv.reader(csvfile, delimiter='\n')
-	    result['features'] = [row.split(',') for row in features]
+	with open('emailDataset/trainFeatures.csv', 'rb') as csvfile:
+		features = csv.reader(csvfile, delimiter=',')
+		result['features'] = [[float(r) for r in row] for row in features]
 
-   	with open('trainLabels.csv', 'rb') as csvfile:
-   		result['labels'] = csv.reader(csvfile, delimiter='\n')
+   	with open('emailDataset/trainLabels.csv', 'rb') as csvfile:
+   		labels = csv.reader(csvfile, delimiter=' ')
+   		result['labels'] = [int(row[0]) for row in labels]
 
 	return result
 
@@ -40,13 +49,13 @@ def Tree():
 def buildDecTree(labels):
 	# labels = list of tuples: (labelVal = 0 or 1, [featureVals])
 
-	same = true
+	same = True
 	stopCondition = len(labels[0][1]) == 1
 	t = Tree()
 
 	for j in range(len(labels)):
-		if labels[j] != labels[i]:
-			same = false
+		if labels[0][0] != labels[j][0]:
+			same = False
 			break;
 
 	if same or stopCondition:
@@ -63,14 +72,13 @@ def buildDecTree(labels):
 		featureSubset = possibleIndices
 
 	maxGain = 0
-	maxThreshold = thresholds[0]
-	maxFeatureI = 0
+	maxThreshold = 0
+	maxFeatureIndex = 0
 
 
 	for j in featureSubset:
 		currF = sorted(set([labels[i][1][j] for i in range(len(labels))])) # values of random feature for len(labels) emails
 		thresholds = [(currF[i] + currF[i+1])/2 for i in range(len(currF) - 1)]
-		
 
 		for k in range(len(thresholds)):
 			t = thresholds[k]
@@ -84,11 +92,11 @@ def buildDecTree(labels):
 		if labels[j][1][maxFeatureIndex] <= maxThreshold:
 			temp = labels[j]
 			temp[1][maxFeatureIndex] = -1
-			leftList.push(temp)
+			leftList.append(temp)
 		else:
 			temp = labels[j]
 			temp[1][maxFeatureIndex] = -1
-			rightList.push(temp)
+			rightList.append(temp)
 
 	left = buildDecTree(leftList)
 	right = buildDecTree(rightList)
@@ -104,44 +112,42 @@ def computeGain(labels, t, featureIndex):
 
 	for i in range(len(labels)):
 		if labels[i][1][featureIndex] <= t:
-			left.push((labels[i][0], labels[i][1]))
+			left.append((labels[i][0], labels[i][1][featureIndex]))
 		else:
-			right.push((labels[i], featureVals[i]))
+			right.append((labels[i][0], labels[i][1][featureIndex]))
 
 	probList = [(labels[i][0], labels[i][1][featureIndex]) for i in range(len(labels))]
 
-	ent = entropy(probList)
-	entL = entropy(left)
-	entR = entropy(right)
+	ent = entropyCalc(probList)
+	entL = entropyCalc(left)
+	entR = entropyCalc(right)
 
-	print(ent)
-	print("\t" + entL)
-	print("\t" + entR)
-	print("\n\n")
-
-	return (ent - (len(left)/len(labels))*entL + (len(right)/len(labels))*entR
+	return ent - (len(left)/len(labels))*entL + (len(right)/len(labels))*entR
 
 	
 
-def entropy(probList):
+def entropyCalc(probList):
 	#probList = tuples of (label, featureVal) for a given feature
 
-	if len(probList == 0):
+	if len(probList) == 0:
 		return 0
 
 	summation = []
 
-	for elem in range(len(probList))):
+	for elem in probList:
 		if elem[0] == 1:
 			P = elem[1]
-			summation.push(P * math.log(P, 2))
+			if (P > 0):
+				summation.append(P * math.log(P, 2))
+			else: 
+				summation.append(0)
 		else:
-			summation.push(0)
+			summation.append(0)
 
 	return 0 - sum(summation)
 
 
 
-
+main(1)
 
 
